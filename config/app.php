60dce9263d6f5fbad1375aa0262a48c808f00f6e
Nikola <nikolaviki@homail.com>
2020-09-26 10:20:53 +0200

    #PIT4-20 initial-stash

--- config/app.php
+++ config/app.php
@@ -162,6 +162,7 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
         Tymon\JWTAuth\Providers\LaravelServiceProvider::class,
+        Intervention\Image\ImageServiceProvider::class,
 
         /*
          * Package Service Providers...
